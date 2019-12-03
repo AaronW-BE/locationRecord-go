e40953e2203964c574afeea6f2c2659bbf95df7c
AaronW <zmtzwb@gmail.com>
2019-12-03 13:59:13 +0800

    补全jwt验证（未加过期时间）及简单认证操作demo

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
 	"errors"
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 	"gopkg.in/yaml.v2"
-	"github.com/dgrijalva/jwt-go"
 	"io/ioutil"
-	"location-record/middleware"
 	"location-record/model"
 	"log"
 	"net/http"
@@ -20,6 +21,11 @@ type Location struct {
 	Lat string `form:"lat" json:"lat" binding:"required"`
 	Lng string `form:"lng" json:"lng" binding:"required"`
 	City string `form:"city" json:"city" binding:"required"`
+}
+
+type User struct {
+	Username string `form:"username" json:"username" binding:"required"`
+	Password string `form:"password" json:"password" binding:"required"`
 }
 
 
@@ -39,14 +45,20 @@ type Config struct {
 func initDB(info *mgo.DialInfo) *mgo.Session {
 	mongo, err := mgo.DialWithInfo(info)
 	if err != nil {
-		log.Fatalf("create mongo session failed %s \n", err)
 		panic("db init failed")
 	}
 	return mongo
 }
 
-func jwtAuth() gin.HandlerFunc {
+func jwtAuth(exclude []string) gin.HandlerFunc {
 	return func(context *gin.Context) {
+
+		for _, url := range exclude {
+			if url == context.FullPath() {
+				context.Next()
+				return
+			}
+		}
 		token := context.Request.Header.Get("Authorization")
 		if token == "" {
 			context.JSON(http.StatusUnauthorized, gin.H{
@@ -70,6 +82,8 @@ func jwtAuth() gin.HandlerFunc {
 				"status": 401,
 				"msg": err.Error(),
 			})
+			context.Abort()
+			return
 		}
 		context.Set("claims", claims)
 		context.Next()
@@ -81,14 +95,14 @@ type JWT struct {
 }
 
 var (
-	TokenExpired error = errors.New("Token is expired")
+	TokenExpired error = errors.New("token is expired")
 	TokenMalformed error = errors.New("token is malformed")
 	TokenInvalid error = errors.New("token is invalid")
 	SignKey string = ""
 )
 
 type CustomChains struct {
-	UID int `json:"uid"`
+	UID string
 	jwt.StandardClaims
 }
 
@@ -107,8 +121,8 @@ func setSignKey(key string) string {
 	return SignKey
 }
 
-func (j *JWT) CreateToken(clains CustomChains) (string, error)  {
-	token := jwt.NewWithClaims(jwt.SigningMethodES256, clains)
+func (j *JWT) CreateToken(claims CustomChains) (string, error)  {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS512, claims)
 	return token.SignedString(j.SingingKey)
 }
 
@@ -138,6 +152,9 @@ func (j *JWT) parseToken(tokenString string)(*CustomChains, error)  {
 }
 
 func main()  {
+
+// ==============================================
+
 	conf := Config{}
 
 	file, err := ioutil.ReadFile(".env.yaml")
@@ -147,6 +164,8 @@ func main()  {
 	}
 
 	configErr := yaml.Unmarshal(file, &conf)
+
+	setSignKey(conf.Jwt.Secret)
 
 	if configErr != nil{
 		panic(configErr)
@@ -162,14 +181,87 @@ func main()  {
 
 	defer mongo.Close()
 
+// =================================================
+
 	router := gin.Default()
 
-	router.Use(middleware.Auth([]string{"/"}))
+	router.Use(jwtAuth([]string{"/auth", "/register"}))
 
 	router.GET("/healthy", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"status": 1,
 		})
+	})
+	
+	router.POST("/register", func(ctx *gin.Context) {
+		var user User
+		if ctx.Bind(&user) == nil {
+			client := mongo.DB("locationRecord").C("users")
+
+			h:= sha1.New()
+			h.Write([]byte(user.Password))
+
+			data := model.User{
+				Username: user.Username,
+				Password: hex.EncodeToString(h.Sum(nil)),
+				RegTime:  time.Now().Unix(),
+			}
+			err := client.Insert(data)
+			if err != nil {
+				log.Println(err)
+				ctx.JSON(http.StatusInternalServerError, gin.H{
+					"message": err.Error(),
+				})
+				return
+			}
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "register success",
+			})
+			return
+		}
+	})
+	
+	router.POST("/auth", func(ctx *gin.Context) {
+		var user User
+		if ctx.Bind(&user) == nil {
+			client := mongo.DB("locationRecord").C("users")
+
+			var userDoc model.User
+
+			h := sha1.New()
+			h.Write([]byte(user.Password))
+
+			_ = client.Find(bson.M{
+				"username": user.Username,
+				"password": hex.EncodeToString(h.Sum(nil)),
+			}).One(&userDoc)
+
+			if userDoc.ID != "" {
+				claims := CustomChains{
+					UID:            userDoc.ID.Hex(),
+				}
+				j := NewJWT()
+				token, err := j.CreateToken(claims)
+				if err != nil {
+					ctx.JSON(http.StatusBadRequest, gin.H{
+						"message": err.Error(),
+					})
+					log.Println(err)
+					return
+				}
+
+				ctx.JSON(http.StatusOK, gin.H{
+					"data": gin.H{
+						"token": token,
+					},
+				})
+				return
+			}
+			ctx.JSON(http.StatusForbidden, gin.H{
+				"message": "授权失败",
+			})
+			return
+		}
 	})
 
 	router.GET("/location", func(ctx *gin.Context) {
@@ -187,6 +279,7 @@ func main()  {
 	})
 
 	router.PUT("/location", func(ctx *gin.Context) {
+		claims :=  ctx.MustGet("claims").(*CustomChains)
 		var json Location
 		if ctx.Bind(&json) == nil {
 			fmt.Printf("lat %s, lng %s, city %s", json.Lat, json.Lng, json.City)
@@ -196,6 +289,7 @@ func main()  {
 				Lng:       json.Lng,
 				Lat:       json.Lat,
 				Timestamp: time.Now().Unix(),
+				Uid:       bson.ObjectIdHex(claims.UID),
 			}
 			err := client.Insert(data)
 			if err != nil {
--- model/location.go
+++ model/location.go
@@ -1,8 +1,17 @@
 package model
 
+import "gopkg.in/mgo.v2/bson"
+
 type Location struct {
 	Lat string
 	Lng string
 	Timestamp int64
-	uid int32
+	Uid bson.ObjectId
+}
+
+type User struct {
+	ID bson.ObjectId `json:"Id,omitempty" bson:"_id,omitempty"`
+	Username string
+	Password string
+	RegTime int64
 }

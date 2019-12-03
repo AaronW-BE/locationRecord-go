3b8e024fefcfb86fb7a92449c0c170b1e2c29348
AaronW <zmtzwb@gmail.com>
2019-12-03 14:16:57 +0800

    reconstruct ready

---
+++ application.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+)
+
+type Application struct {
+	App *gin.Engine
+	DB struct{
+		MongoDB interface{}
+	}
+	Config Config
+}
+
+func (app *Application) Init(config Config)  {
+	app.Config = config
+}
+
+func New() *Application  {
+	return &Application{}
+}
+
+func main()  {
+	application := New()
+
+	config := Config{}
+	file, err := ioutil.ReadFile(".env.yaml")
+	if err != nil {
+		panic(err)
+	}
+	configErr := yaml.Unmarshal(file, &config)
+	if configErr != nil{
+		panic(configErr)
+	}
+
+	application.Init(config)
+
+}
